7c6c10ca8eb40438ef9f9f970c376135eb2e9e25
Emtiaz Chowdhury <emtiazchow@gmail.com>
2025-01-01 19:47:52 -0500

    Maven Project

---
+++ src/test/java/regressionTest.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+public class regressionTest {
+
+    @Test
+    public void test(){
+        System.out.println("Testing");
+
+    }
+
+
+}
---
+++ src/test/java/smokeTest.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class smokeTest {
+
+    @Test
+    public void sampleTest() {
+        System.out.println("Running unit test...");
+    }
+}
